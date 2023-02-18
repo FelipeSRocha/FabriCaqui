6a7fb067b39b22d144eeb977a5014f7df125550d
FelipeSRocha <62608574+FelipeSRocha@users.noreply.github.com>
2023-02-18 20:20:16 -0300

    Update Rest Function

--- utils/rest.tsx
+++ utils/rest.tsx
@@ -1,5 +1,4 @@
-interface props {route:string, method?:string, payload?:object}
-const RESTAPI = async ({route, method="GET", payload={}}: props) =>{
+const RESTAPI = async (route:string, method:string="GET", payload:object={}) =>{
     if(method && route && method==="GET"){
         try{
             const response = await fetch('/api/'+route)
