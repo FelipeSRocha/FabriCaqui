9131f9ebdf49cbc8d11cf99826bdbba8715bdb59
FelipeSRocha <62608574+FelipeSRocha@users.noreply.github.com>
2023-06-09 18:14:07 -0300

    Filling the factory detail

--- components/popup/DetailPopup.tsx
+++ components/popup/DetailPopup.tsx
@@ -2,6 +2,15 @@ import { useSpring, animated } from "react-spring"
 import { useEffect, useState } from "react"
 import { factory } from "../../utils/types/types"
 import MainMap, { coordType } from "../Maps/MainMap"
+
+import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace"
+import { Label, Title } from "../Text/TextStandarts"
+
+const LabelBox = ({children}:{children: any}) => (
+    <div className="flex flex-row items-center">
+        {children}
+    </div>
+)
 const DetailPopup = ({
     externalState,
     factoryDetail,
@@ -25,29 +34,51 @@ const DetailPopup = ({
                 lng: factoryDetail?.location.coordinates[0],
             })
         }
-    })
+    },[factoryDetail])
     const closeElement = () => {
         setInternState(false)
         setFactoryDetail(undefined)
     }
-    // useEffect(() => {
-    //     if (externalState) {
-    //         setInternState(externalState)
-    //     }
-    // }, [externalState])
 
     return factoryDetail ? (
         <div className="z-30 w-full h-full left-0 bottom-0 absolute self-center flex items-center justify-center bg-black bg-opacity-50">
             <div className="flex flex-col md:flex-row w-full h-full border-x-[1px] border-purple-main relative">
                 <div
-                    className={`lg:w-3/5 lg:pl-4 md:h-full h-1/2 lg:block w-screen left-0 top-0 bg-purple-main`}
+                    className={`lg:w-3/5 lg:pl-4 lg:p-4 md:h-full h-1/2 lg:block w-screen left-0 top-0 bg-white relative flex flex-col`}
                 >
                     <span
-                        className="p-4 bg-white box-border cursor-pointer"
+                        className="p-4 bg-white box-border cursor-pointer flex flex-row items-center"
                         onClick={closeElement}
                     >
-                        X
+                        <KeyboardBackspaceIcon fontSize="large" />
+                        <h1 className="block text-xl font-medium text-gray-700 pl-4">Voltar</h1>
                     </span>
+                    <div className="flex flex-col gap-4">
+                        <LabelBox >
+                            <Label text={"Empresa: "}></Label>
+                            <Title
+                                text={factoryDetail.general.factoryName}
+                            ></Title>
+                        </LabelBox>
+                        <LabelBox >
+                            <Label text={"Endereço: "}></Label>
+                            <Title
+                                text={`${factoryDetail.address.logradouro}, ${factoryDetail.address.numero}, ${factoryDetail.address.bairro} - ${factoryDetail.address.localidade}/${factoryDetail.address.uf}`}
+                            ></Title>
+                        </LabelBox>
+                        <LabelBox >
+                            <Label text={"Telefone: "}></Label>
+                            <Title
+                                text={`${factoryDetail.general.phoneContact}`}
+                            ></Title>
+                        </LabelBox>
+                        <LabelBox >
+                            <Label text={"Email: "}></Label>
+                            <Title
+                                text={`${factoryDetail.general.emailContact}`}
+                            ></Title>
+                        </LabelBox>
+                    </div>
                 </div>
                 <div
                     className={`lg:w-2/5 h-1/2 md:h-full lg:block lg:border-l-4 border-purple-main w-full`}
